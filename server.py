58154861d6f543fb13224180c712b5e9f1935a57
Aek-dev-ai <zorotrader0@gmail.com>
2025-07-24 11:43:06 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import datetime
+import os
+
 
 app = Flask(__name__)
 
@@ -27,5 +29,6 @@ def verify():
     return jsonify({"success": True, "token": token})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
